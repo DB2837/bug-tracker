31825c249f306d76584b935478ed57bad1f2502f
davide <davidebellucci975@gmail.com>
2022-07-15 20:28:53 +0200

    db setup

---
+++ server/src/data-source.ts
@@ -0,0 +1,20 @@
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const AppDataSource: DataSource = new DataSource({
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: false,
+  logging: false,
+  entities: ['src/models/**/*{.js,.ts}'],
+  migrations: ['src/migrations/**/*{.js,.ts}'],
+  subscribers: [],
+  migrationsTableName: 'migrations',
+});
---
+++ server/src/migration/1657907197245-myMigration.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class myMigration1657907197245 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
