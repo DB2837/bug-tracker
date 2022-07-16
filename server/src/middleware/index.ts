df9c01019f0f092349ec9936513399ec24ce225c
davide <davidebellucci975@gmail.com>
2022-07-16 17:39:26 +0200

    add entry point file to export functions

---
+++ server/src/middleware/index.ts
@@ -0,0 +1,5 @@
+import { errorHandler } from './errorHandler';
+import { notFound } from './notFound';
+import { verifyJwt } from './verifyJWT';
+
+export default { errorHandler, notFound, verifyJwt };
